656658f3a3d1def435b21fdced811b304c2421a2
Derrick Strong <derrickmstrong@gmail.com>
2021-09-27 15:21:04 -0500

    Toggle language

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const App = () => {
-    return (
-        <div className="ui container">
-            Hello App
-        </div>
-    )
-}
+  const [langauge, setLanguage] = useState('english');
+  return (
+    <div className='ui container'>
+      <div>
+        Select a langauge:{' '}
+        <i className='flag us' onClick={() => setLanguage('english')}></i>
+        <i className='flag nl' onClick={() => setLanguage('dutch')}></i>
+      </div>
+      {langauge}
+    </div>
+  );
+};
 
-export default App
+export default App;
