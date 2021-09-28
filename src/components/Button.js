279eca6b7ea0f748ccf89ca4ba0b4dec4c605b20
Derrick Strong <derrickmstrong@gmail.com>
2021-09-28 06:05:29 -0500

    Refactored Btn from Provider to Consumer context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,31 @@
+// Using Provider - for a single context object
+// import React from 'react';
+// import LanguageContext from '../contexts/LanguageContext';
+
+// const Button = () => {
+//     // console.log("Language:", Button.contextType._currentValue)
+//     const language = Button.contextType._currentValue;
+
+//     const text = language === 'english' ? 'Submit' : 'Voorleggen';
+    
+//     return <button className='ui button primary'>{text}</button>;
+// };
+
+// export default Button;
+// Button.contextType = LanguageContext;
+
+// Using Consumer - for multiple context objects
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
-const Button = () => {
-    // console.log("Language:", Button.contextType._currentValue)
-    
-    const text =
-    Button.contextType._currentValue === 'english' ? 'Submit' : 'Voorleggen';
-    
-    return <button className='ui button primary'>{text}</button>;
+const Button = () => {    
+    return (
+      <button className='ui button primary'>
+        <LanguageContext.Consumer>
+          {value => value === 'english' ? 'Submit' : 'Voorleggen'}
+        </LanguageContext.Consumer>
+      </button>
+    );
 };
 
 export default Button;
-
-Button.contextType = LanguageContext;
