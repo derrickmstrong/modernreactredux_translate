88d56247424c229b2d79bbf604cb3ec700ce0ba2
Derrick Strong <derrickmstrong@gmail.com>
2021-09-28 06:06:05 -0500

    Format changes

--- src/components/Field.js
+++ src/components/Field.js
@@ -3,9 +3,9 @@ import LanguageContext from '../contexts/LanguageContext';
 
 const Field = () => {
     // console.log("Language:", Field.contextType._currentValue)
+    const language = Field.contextType._currentValue;
     
-    const text =
-    Field.contextType._currentValue === 'english' ? 'Name' : 'Naam';
+    const text = language === 'english' ? 'Name' : 'Naam';
     return (
         <div className="ui field">
             <label htmlFor={text}>{text}</label>
