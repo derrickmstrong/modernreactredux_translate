130f921070c34f8d2f042e8d6730f3ac43297af4
Derrick Strong <derrickmstrong@gmail.com>
2021-09-28 06:27:09 -0500

    Correction Update Btn from contextType to Consumer

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 const App = () => {
   const [langauge, setLanguage] = useState('english');
+  const [color, setColor] = useState('primary')
+
+  const handleUS = () => {
+      setLanguage('english');
+      setColor('primary');
+  }
+
+  const handleNL = () => {
+      setLanguage('dutch');
+      setColor('negative');
+  }
+
   return (
     <div className='ui container'>
       <div>
         Select a langauge:{' '}
-        <i className='flag us' onClick={() => setLanguage('english')}></i>
-        <i className='flag nl' onClick={() => setLanguage('dutch')}></i>
+        <i className='flag us' onClick={handleUS}></i>
+        <i className='flag nl' onClick={handleNL}></i>
+        <br />
+        <br />
         <LanguageContext.Provider value={langauge}>
-          <UserCreate />
+          <ColorContext.Provider value={color}>
+            <UserCreate />
+          </ColorContext.Provider>
         </LanguageContext.Provider>
       </div>
     </div>
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,4 @@
-// Using Provider - for a single context object
+// Using contextType._currentValue - for a single context object
 // import React from 'react';
 // import LanguageContext from '../contexts/LanguageContext';
 
@@ -7,25 +7,33 @@
 //     const language = Button.contextType._currentValue;
 
 //     const text = language === 'english' ? 'Submit' : 'Voorleggen';
-    
+
 //     return <button className='ui button primary'>{text}</button>;
 // };
 
 // export default Button;
 // Button.contextType = LanguageContext;
 
+
+
 // Using Consumer - for multiple context objects
+// Consumer objects must return a function
 import React from 'react';
+import ColorContext from '../contexts/ColorContext';
 import LanguageContext from '../contexts/LanguageContext';
 
-const Button = () => {    
-    return (
-      <button className='ui button primary'>
-        <LanguageContext.Consumer>
-          {value => value === 'english' ? 'Submit' : 'Voorleggen'}
-        </LanguageContext.Consumer>
-      </button>
-    );
+const Button = () => {
+  return (
+    <ColorContext.Consumer>
+      {(color) => (
+        <button className={`ui button ${color}`}>
+          <LanguageContext.Consumer>
+            {language => (language === 'english' ? 'Submit' : 'Voorleggen')}
+          </LanguageContext.Consumer>
+        </button>
+      )}
+    </ColorContext.Consumer>
+  );
 };
 
 export default Button;
